2ed4b11721d275d0b0eda19f82c8e18a6db9082d
Oliwerivs <olda.broz@gmail.com>
2023-11-07 21:27:45 +0000

    Druhy pokusny GIT commit

---
+++ Python/Paint_calc.py
@@ -0,0 +1,14 @@
+import math
+#1 plechovka 5m2
+vyska = int(input("Vyska zdi:\n"))
+sirka = int(input("Sirka zdi:\n"))
+pokryti = 5
+
+def kolik_barvy(vyska,sirka,pokryti):
+    plocha = vyska * sirka
+    plechovky = math.ceil(plocha /pokryti)
+    print(f"Budete potrebovat {plechovky} plechovek barvy.")
+    
+kolik_barvy(vyska,sirka,pokryti)
+
+
